ec5c399ba8741ab0aa00baaa32954cfab0c48477
francisco gil <franciscomarcelogil@gmail.com>
2025-07-12 17:42:20 -0300

    orm app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
-import express from 'express'
-import { deporteRouter } from './deporte/deporte.routes.js'
-import { establecimientoRouter } from './establecimiento/establecimiento.routes.js'
+import 'reflect-metadata';
+import express from 'express';
+import { deporteRouter } from './deporte/deporte.routes.js';
+import { establecimientoRouter } from './establecimiento/establecimiento.routes.js';
+import { orm, syncSchema } from './shared/db/orm.js';
+import { RequestContext } from '@mikro-orm/core';
+const app = express();
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
+app.use((req, res, next) => {
+  RequestContext.create(orm.em, next);
+});
+app.use('/api/deportes', deporteRouter);
 
-app.use('/api/deportes', deporteRouter)
-
-app.use('/api/establecimientos', establecimientoRouter)
+app.use('/api/establecimientos', establecimientoRouter);
 
 app.use((_, res) => {
-  res.status(404).send({ message: 'Resource not found' })
-})
+  res.status(404).send({ message: 'Resource not found' });
+});
+
+await syncSchema();
 
 app.listen(3000, () => {
-  console.log('Server runnning on http://localhost:3000/')})
+  console.log('Server runnning on http://localhost:3000/');
+});
