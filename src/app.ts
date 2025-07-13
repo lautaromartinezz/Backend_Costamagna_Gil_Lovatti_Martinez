ac55e055f4b386789d7a75480a2b622607ed5b90
franlovatti <franciscolovatti08@gmail.com>
2025-07-13 00:09:06 -0300

    noticia crud

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { deporteRouter } from './deporte/deporte.routes.js';
 import { establecimientoRouter } from './establecimiento/establecimiento.routes.js';
 import { orm, syncSchema } from './shared/db/orm.js';
 import { RequestContext } from '@mikro-orm/core';
+import { noticiaRouter } from './noticia/noticia.routes.js';
 const app = express();
 app.use(express.json());
 
@@ -13,6 +14,8 @@ app.use((req, res, next) => {
 app.use('/api/deportes', deporteRouter);
 
 app.use('/api/establecimientos', establecimientoRouter);
+
+app.use('/api/noticias', noticiaRouter);
 
 app.use((_, res) => {
   res.status(404).send({ message: 'Resource not found' });
