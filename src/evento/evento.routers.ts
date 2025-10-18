ff4923b6e5063cda376304b0df1326a7349594f6
franlovatti <franciscolovatti08@gmail.com>
2025-10-18 02:49:37 -0300

    fix merge

--- src/evento/evento.routers.ts
+++ src/evento/evento.routers.ts
@@ -6,13 +6,14 @@ import {
   add,
   update,
   remove,
+  findSome
 } from './evento.controller.js';
 import { authMiddleware } from '../shared/authMiddleware.js';
 
 export const eventoRouter = Router();
 
 eventoRouter.get('/', findAll);
-
+eventoRouter.get('/filter', findSome);
 eventoRouter.get('/:id', findOne);
 eventoRouter.post('/', authMiddleware, sanitizeEventoInput, add);
 eventoRouter.put('/:id', authMiddleware, sanitizeEventoInput, update);
